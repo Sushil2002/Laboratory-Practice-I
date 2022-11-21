4de562b296dfa550d74927f19de01dbf0ce09eaf
Sushilkumar Dhamane <103381144+Sushil2002@users.noreply.github.com>
2022-11-21 11:37:31 +0530

    Update Allfit.cpp

--- Assignment4/Allfit.cpp
+++ Assignment4/Allfit.cpp
@@ -3,10 +3,7 @@
 typedef long long ll;
 using namespace std;
 // ASCII of 'a'-97,'z'-123,'A'-65,'Z'-90,'0'-48,'9'-57
-bool sortby(const pair<ll, ll> &a, const pair<ll, ll> &b)
-{
-    return a.second < b.second;
-}
+
 void First(int Blocksize[], int m, int ProcessSize[], int n)
 {
     int allocation[n];
