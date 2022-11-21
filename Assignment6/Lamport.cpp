3fac57ad40574920372a230d21e6b11bf1f346d5
Sushilkumar Dhamane <103381144+Sushil2002@users.noreply.github.com>
2022-11-21 19:05:13 +0530

    Update Lamport.cpp

--- Assignment6/Lamport.cpp
+++ Assignment6/Lamport.cpp
@@ -1,77 +1,113 @@
+
+
 #include <bits/stdc++.h>
-#include <iostream>
-typedef long long ll;
 using namespace std;
-// ASCII of 'a'-97,'z'-123,'A'-65,'Z'-90,'0'-48,'9'-57
-bool sortby(const pair<ll, ll> &a, const pair<ll, ll> &b)
+
+int max1(int a, int b)
 {
-    return a.second < b.second;
+
+    if (a > b)
+        return a;
+    else
+        return b;
 }
-void LamportLogicalClock(int e1, int e2, int m[5][3])
+void display(int e1, int e2,
+             int p1[5], int p2[3])
 {
-    int p1[e1];
-    int p2[e2];
-    for (int i = 0; i < e1; i++)
-    {
-        p1[i] = i + 1;
-    }
-    for (int i = 0; i < e2; i++)
-    {
-        p2[i] = i + 1;
-    }
-    for (int i = 0; i < e1; i++)
-    {
-        for (int j = 0; j < e2; j++)
-        {
-            if (m[i][j] == 1)
-            {
-                p2[j] = max(p2[j], p1[i] + 1);
-                for (int k = j + 1; k < e2; k++)
-                {
-                    p2[k] = p2[k - 1] + 1;
-                }
-            }
-            if (m[i][j] == -1)
-            {
-                p1[i] = max(p1[i], p2[j] + 1);
-                for (int k = i + 1; k < e1; k++)
-                {
-                    p1[k] = p2[k - 1] + 1;
-                }
-            }
-        }
-    }
-    cout << "Time Stamp for Proecess P1 : " << endl;
-    for (int i = 0; i < e1; i++)
+    int i;
+    cout << "\nThe time stamps of "
+            "events in P1:\n";
+
+    for (i = 0; i < e1; i++)
     {
         cout << p1[i] << " ";
     }
-    cout << "\n\nTime Stamp for Proecess P1 : " << endl;
-    for (int i = 0; i < e2; i++)
-    {
+    cout << "\nThe time stamps of "
+            "events in P2:\n";
+
+    for (i = 0; i < e2; i++)
         cout << p2[i] << " ";
-    }
 }
+
+void lamportLogicalClock(int e1, int e2,
+                         int m[5][5])
+{
+    int i, j, k, p1[e1], p2[e2];
+
+    for (i = 0; i < e1; i++)
+        p1[i] = i + 1;
+
+    for (i = 0; i < e2; i++)
+        p2[i] = i + 1;
+    cout << "\t";
+    for (i = 0; i < e2; i++)
+        cout << "\te2" << i + 1;
+
+    for (i = 0; i < e1; i++)
+    {
+
+        cout << "\n e1" << i + 1 << "\t";
+
+        for (j = 0; j < e2; j++)
+            cout << m[i][j] << "\t";
+    }
+
+    for (i = 0; i < e1; i++)
+    {
+        for (j = 0; j < e2; j++)
+        {
+            if (m[i][j] == 1)
+            {
+                p2[j] = max1(p2[j], p1[i] + 1);
+                for (k = j + 1; k < e2; k++)
+                    p2[k] = p2[k - 1] + 1;
+            }
+            if (m[i][j] == -1)
+            {
+                p1[i] = max1(p1[i], p2[j] + 1);
+                for (k = i + 1; k < e1; k++)
+                    p1[k] = p1[k - 1] + 1;
+            }
+        }
+    }
+
+    display(e1, e2, p1, p2);
+}
+
 int main()
 {
-    int e1 = 5, e2 = 3, m[5][3];
+    int e1 = 5, e2 = 5, m[5][5];
     m[0][0] = 0;
-	m[0][1] = 0;
-	m[0][2] = 0;
-	m[1][0] = 0;
-	m[1][1] = 0;
-	m[1][2] = 1;
-	m[2][0] = 0;
-	m[2][1] = 0;
-	m[2][2] = 0;
-	m[3][0] = 0;
-	m[3][1] = 0;
-	m[3][2] = 0;
-	m[4][0] = 0;
-	m[4][1] = -1;
-	m[4][2] = 0;
+    m[0][1] = 0;
+    m[0][2] = 1;
+    m[0][3] = 0;
+    m[0][4] = 0;
 
-	// Function Call
-	LamportLogicalClock(e1, e2, m);
+    m[1][0] = -1;
+    m[1][1] = 0;
+    m[1][2] = 0;
+    m[1][3] = 0;
+    m[1][4] = 0;
+
+    m[2][0] = 0;
+    m[2][1] = 1;
+    m[2][2] = 0;
+    m[2][3] = 0;
+    m[2][4] = 0;
+
+    m[3][0] = 0;
+    m[3][1] = 0;
+    m[3][2] = 0;
+    m[3][3] = 0;
+    m[3][4] = 1;
+
+    m[4][0] = 0;
+    m[4][1] = 0;
+    m[4][2] = 0;
+    m[4][3] = 0;
+    m[4][4] = 1;
+
+    lamportLogicalClock(e1, e2, m);
+
     return 0;
 }
